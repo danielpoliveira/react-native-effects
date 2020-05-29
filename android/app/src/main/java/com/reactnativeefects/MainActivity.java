6d1d6ed5ed1daff629e9d00c2fde4e8807b98e52
danielpolivera <dani.edm@outlook.com>
2020-05-29 10:46:15 -0300

    Added configurations on react-native-handle-gestures

--- android/app/src/main/java/com/reactnativeefects/MainActivity.java
+++ android/app/src/main/java/com/reactnativeefects/MainActivity.java
@@ -1,6 +1,9 @@
 package com.reactnativeefects;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;                           // + react-native-gesture-handler
+import com.facebook.react.ReactRootView;                                   // + react-native-gesture-handler
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView; // + react-native-gesture-handler
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +15,14 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "reactNativeEfects";
   }
+
+  @Override                                                             // + react-native-gesture-handler
+  protected ReactActivityDelegate createReactActivityDelegate() {       // + react-native-gesture-handler
+    return new ReactActivityDelegate(this, getMainComponentName()) {    // + react-native-gesture-handler
+      @Override                                                         // + react-native-gesture-handler
+      protected ReactRootView createRootView() {                        // + react-native-gesture-handler
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);   // + react-native-gesture-handler
+      }                                                                 // + react-native-gesture-handler
+    };                                                                  // + react-native-gesture-handler
+  }                                                                     // + react-native-gesture-handler
 }
